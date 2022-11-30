d91f12975cce5578ca6f49b8c1bcd8be14c29f36
Tom <tomcodedthis@gmail.com>
2022-11-30 11:08:14 +0000

    Fix: removed unused loader component

--- components/Scene.tsx
+++ components/Scene.tsx
@@ -5,26 +5,25 @@ import Lambo from "./Models/Lambo";
 import Porsche from "./Models/Porsche";
 import Datsun from "./Models/Datsun";
 
-
 export default function Scene(props: any) {
   return (
     <main className={"w-[100%] h-[90vh] bg-white"}>
       <Suspense fallback={<Loader />}>
         <Canvas frameloop="demand">
-            <ambientLight intensity={4} />
-            <pointLight position={[0, 2, 10]} intensity={1.5} />
-            <pointLight position={[0, 2, -10]} intensity={1.5} />
+          <ambientLight intensity={4} />
+          <pointLight position={[0, 2, 10]} intensity={1.5} />
+          <pointLight position={[0, 2, -10]} intensity={1.5} />
 
-            <PerspectiveCamera makeDefault position={[2, 2, 12]} />
-            <OrbitControls rotateSpeed={0.4} autoRotate={props.play} />
-            
-              {props.currentModel === 0 ? (
-                <Datsun {...props} />
-              ) : props.currentModel === 1 ? (
-                <Lambo {...props} />
-              ) : (
-                <Porsche {...props} />
-              )}
+          <PerspectiveCamera makeDefault position={[2, 2, 12]} />
+          <OrbitControls rotateSpeed={0.4} autoRotate={props.play} />
+
+          {props.currentModel === 0 ? (
+            <Datsun {...props} />
+          ) : props.currentModel === 1 ? (
+            <Lambo {...props} />
+          ) : (
+            <Porsche {...props} />
+          )}
         </Canvas>
       </Suspense>
     </main>
